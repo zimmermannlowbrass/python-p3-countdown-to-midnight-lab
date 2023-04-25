ded740bab50c69122ea91a2898da06067b07360a
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-25 12:36:55 -0500

    completed labwork

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+def countdown(x):
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x += -1
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(x):
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x += -1
+    print('HAPPY NEW YEAR!')
